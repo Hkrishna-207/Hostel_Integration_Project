2d2300eb1a09f887bf5fb5fdeed9e79179641289
KRISHNA <harikrishnaburada11@gmail.com>
2026-01-14 13:16:01 +0530

    Remove operation success in Admin

--- src/main/java/com/hstls/intigration/controller/AdminController.java
+++ src/main/java/com/hstls/intigration/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -65,8 +66,9 @@ public class AdminController {
 		
 	}
 	
-	@GetMapping("/removehostel")
-	public String removeHostel() {
+	@GetMapping("/removehostel/{id}")
+	public String removeHostel(@PathVariable Long id) {
+		hstlRepo.deleteById(id);
 		return "redirect:/admin/dashboard";
 	}
 }
